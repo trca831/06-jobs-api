13c9b1bf72a95c45ff4e0d44a42301eb1b3edf7b
Tracy Cano <tracycano3.14@gmail.com>
2025-05-31 14:31:23 -0700

    first part of assignment

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -60,7 +60,7 @@ const deleteJob = async (req, res) => {
     if (!job) {
         throw new NotFoundError(`No job with id ${jobId}`)
     }
-    res.status(StatusCodes.OK).send()
+    res.status(StatusCodes.OK).json({ msg: "The entry was deleted." });
 }
 
 
